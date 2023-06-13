93662cf77f714fa80384f2e60bd7df2bdf1d8e7a
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-06-13 15:37:30 -0500

    labs pased

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,15 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
+    #GET request to the URL passed in on initialization. 
+    # This method should return the body of the response.
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
+    
+
+# get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# get_requester.load_json()
